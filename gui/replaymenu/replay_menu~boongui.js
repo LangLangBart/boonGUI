2b53a25103c93252405de78616dc970f8cd4d1c6
Langbart <92653266+LangLangBart@users.noreply.github.com>
2021-11-07 21:51:52 +0100

    adding a button next to the replay file path to quickly open the directory

---
+++ gui/replaymenu/replay_menu~boongui.js
@@ -0,0 +1,44 @@
+/**
+ * Shows preview image, description and player text in the right panel.
+ */
+function displayReplayDetails()
+{
+	let selected = Engine.GetGUIObjectByName("replaySelection").selected;
+	let replaySelected = selected > -1;
+
+	Engine.GetGUIObjectByName("replayInfo").hidden = !replaySelected;
+	Engine.GetGUIObjectByName("replayInfoEmpty").hidden = replaySelected;
+	Engine.GetGUIObjectByName("startReplayButton").enabled = replaySelected;
+	Engine.GetGUIObjectByName("deleteReplayButton").enabled = replaySelected;
+	Engine.GetGUIObjectByName("openReplayDirectoryButton").hidden = !replaySelected;
+	Engine.GetGUIObjectByName("replayFilename").hidden = !replaySelected;
+	Engine.GetGUIObjectByName("summaryButton").hidden = true;
+
+	if (!replaySelected)
+		return;
+
+	let replay = g_ReplaysFiltered[selected];
+
+	Engine.GetGUIObjectByName("sgMapName").caption = translate(replay.attribs.settings.Name);
+	Engine.GetGUIObjectByName("sgMapSize").caption = translateMapSize(replay.attribs.settings.Size);
+	Engine.GetGUIObjectByName("sgMapType").caption = translateMapType(replay.attribs.mapType);
+	Engine.GetGUIObjectByName("sgVictory").caption = replay.attribs.settings.VictoryConditions.map(victoryConditionName =>
+		translateVictoryCondition(victoryConditionName)).join(translate(", "));
+	Engine.GetGUIObjectByName("sgNbPlayers").caption = sprintf(translate("Players: %(numberOfPlayers)s"),
+		{ "numberOfPlayers": replay.attribs.settings.PlayerData.length });
+	Engine.GetGUIObjectByName("replayFilename").caption = Engine.GetReplayDirectoryName(replay.directory);
+
+	let metadata = Engine.GetReplayMetadata(replay.directory);
+	Engine.GetGUIObjectByName("sgPlayersNames").caption =
+		formatPlayerInfo(
+			replay.attribs.settings.PlayerData,
+			Engine.GetGUIObjectByName("showSpoiler").checked &&
+				metadata &&
+				metadata.playerStates &&
+				metadata.playerStates.map(pState => pState.state));
+
+	Engine.GetGUIObjectByName("sgMapPreview").sprite = g_MapCache.getMapPreview(replay.attribs.mapType, replay.attribs.map, replay.attribs?.mapPreview);
+	Engine.GetGUIObjectByName("sgMapDescription").caption = g_MapCache.getTranslatedMapDescription(replay.attribs.mapType, replay.attribs.map);
+
+	Engine.GetGUIObjectByName("summaryButton").hidden = !Engine.HasReplayMetadata(replay.directory);
+}
