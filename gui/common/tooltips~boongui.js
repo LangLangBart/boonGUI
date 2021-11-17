2a36aa53c8348de0126eb8f7e6fa3ac7434706a9
Langbart <92653266+LangLangBart@users.noreply.github.com>
2021-11-17 17:39:28 +0100

    better use isFinite

--- gui/common/tooltips~boongui.js
+++ gui/common/tooltips~boongui.js
@@ -40,7 +40,7 @@ switch (true) {
 	case (isNaN(num)):
 		return '';
     	break;
-	case (num == 1/0):
+	case (isFinite(num)):
 		return translate("\u221E");
     	break;
 	case (num == 0 || num >= 10):
