0ef48688234a124a8cef722a68290ee1e5e35b4a
Derek Owumi <derekowumi@gmail.com>
2021-11-20 17:51:46 -0600

    Create quick-installerOSX.sh

---
+++ quick-installerOSX.sh
@@ -0,0 +1,15 @@
+!/bin/bash
+
+
+echo "Running Installation Script"
+pwd
+cd ~/Library/Application\Support/0ad/mods/
+
+
+git clone https://github.com/LangLangBart/boonGUI.git
+
+echo "installation complete running 0 A.D."
+
+0ad
+
+exit
