eadd7a146d484a6ee8f78e478adbb9a1751025a8
Derek Owumi <derekowumi@gmail.com>
2021-11-22 00:06:38 -0600

    Update quick-installerOSX.sh

--- quick-installerOSX.sh
+++ quick-installerOSX.sh
@@ -1,16 +1,14 @@
-#!/bin/zsh
+#!/bin/sh
 
 
 
 echo "Running Installation Script"
-pwd
-cd ~/Library/Application\Support/0ad/mods/
+# Changes Dir to 0 A.Ds default installation Dir
+cd ~/Library/Application\ Support/0ad/mods/
 
-
+#clones the latest version of BoonGui
 git clone https://github.com/LangLangBart/boonGUI.git
-
-echo "installation complete running 0 A.D."
-
-0ad
-
+#tells user it has been installed
+echo "installation complete you may run 0 A.D. now"
+#ends programme
 exit
