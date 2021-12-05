1c89a640ccbb6a9baa85a5d205a487d00e561c5b
Langbart <92653266+LangLangBart@users.noreply.github.com>
2021-12-05 14:05:27 +0100

    Fix #66 and layout adjustments for #67

--- gui/session/stats/BoonGUIStatsModes.js
+++ gui/session/stats/BoonGUIStatsModes.js
@@ -1,4 +1,5 @@
-class BoonGUIStatsModes {
+class BoonGUIStatsModes
+{
 
 	static Modes = [
 		{ "type": "production", "icon": "cpu", "title": "Production" },
@@ -13,7 +14,8 @@ class BoonGUIStatsModes {
 		// { type: "scores", icon: "trophy", title: "Scores" },
 	];
 
-	constructor(forceRender) {
+	constructor(forceRender)
+	{
 		const PREFIX = "StatsModes";
 		this.root = Engine.GetGUIObjectByName(PREFIX);
 		this.modeIndex = 0;
@@ -25,10 +27,11 @@ class BoonGUIStatsModes {
 		this.rowsContainer = Engine.GetGUIObjectByName(`${PREFIX}Rows`);
 		this.rows = this.rowsContainer.children.map((row, index) => new BoonGUIStatsModesRow(row, index));
 
-		this.title.size = '0 0 100%-3 28';
+		this.title.size = '10 0 100% 28';
 		this.title.sprite = "backcolor: 38 38 38 150";
 
-		this.tabButtons.size = '100%-50 30 100% 100%';
+		this.tabButtons.size = '100%-47 28 100% 121%';
+		this.tabButtons.sprite = "backcolor: 38 38 38 150";
 		this.rowsContainer.size = '0 30 100%-55 100%';
 
 		this.forceRender = forceRender;
@@ -39,11 +42,13 @@ class BoonGUIStatsModes {
 		g_OverlayCounterManager.registerResizeHandler(this.setTopOffset.bind(this));
 	}
 
-	setTopOffset(offset) {
+	setTopOffset(offset)
+	{
 		this.root.size = `100%-199 ${155 + offset} 100% 500`;
 	}
 
-	selectMode(modeIndex) {
+	selectMode(modeIndex)
+	{
 		const mode = BoonGUIStatsModes.Modes[modeIndex];
 		if (!mode) return;
 		this.modeIndex = modeIndex;
@@ -52,45 +57,24 @@ class BoonGUIStatsModes {
 		this.forceRender();
 	}
 
-	previousMode(limit = false) {
+	previousMode(limit = false)
+	{
 		const next = this.modeIndex - 1;
 		const outOfLimit = next < 0;
 		if (outOfLimit && limit) return;
 		this.selectMode(outOfLimit ? BoonGUIStatsModes.Modes.length - 1 : next);
 	}
 
-	nextMode(limit = false) {
+	nextMode(limit = false)
+	{
 		const next = this.modeIndex + 1;
 		const outOfLimit = next >= BoonGUIStatsModes.Modes.length;
 		if (outOfLimit && limit) return;
 		this.selectMode(outOfLimit ? 0 : next);
 	}
 
-	update(playersStates) {
+	update(playersStates)
+	{
 		this.rows.forEach((row, i) => row.update(playersStates[i], BoonGUIStatsModes.Modes[this.modeIndex].type));
-	}
-}
-
-class BoonGUIStatsModesTab {
-	constructor(tab, index, parent) {
-		const mode = BoonGUIStatsModes.Modes[index] ?? null;
-		this.tab = tab;
-		this.index = index;
-		if (mode)
-		{
-			tab.hidden = false;
-			tab.size = BoonGUIGetRowSize(index, 40);
-			const text = Engine.GetGUIObjectByName(`${tab.name}_Text`);
-			const icon = Engine.GetGUIObjectByName(`${tab.name}_Icon`);
-			icon.sprite = `stretched:session/phosphor/${mode.icon}.png`;
-			tab.tooltip = colorizeHotkey(`${mode.title} %(hotkey)s`, `boongui.session.stats.mode.${index + 1}`);
-			tab.onPress = () => {
-				parent.selectMode(index);
-			};
-		}
-	}
-
-	update(modeIndex) {
-		this.tab.enabled = modeIndex !== this.index;
 	}
 }
---
+++ gui/session/stats/BoonGUIStatsModesTab.js
@@ -0,0 +1,26 @@
+class BoonGUIStatsModesTab
+{
+	constructor(tab, index, parent)
+	{
+		const mode = BoonGUIStatsModes.Modes[index] ?? null;
+		this.tab = tab;
+		this.index = index;
+		if (mode)
+		{
+			tab.hidden = false;
+			tab.size = BoonGUIGetRowSize(index, 37);
+			const text = Engine.GetGUIObjectByName(`${tab.name}_Text`);
+			const icon = Engine.GetGUIObjectByName(`${tab.name}_Icon`);
+			icon.sprite = `stretched:session/phosphor/${mode.icon}.png`;
+			tab.tooltip = colorizeHotkey(`${mode.title} %(hotkey)s`, `boongui.session.stats.mode.${index + 1}`);
+			tab.onPress = () => {
+				parent.selectMode(index);
+			};
+		}
+	}
+
+	update(modeIndex)
+	{
+		this.tab.enabled = modeIndex !== this.index;
+	}
+}
