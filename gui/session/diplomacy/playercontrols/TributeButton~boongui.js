687f84be644da14f0c84598d07507d221f434387
Langbart <92653266+LangLangBart@users.noreply.github.com>
2022-01-19 10:27:27 +0100

    Fixes #84 by adding a checkbox to the tribute section
    It is switched off by default, as in most cases you do not want to contribute to your enemies.

--- gui/session/diplomacy/playercontrols/TributeButton~boongui.js
+++ gui/session/diplomacy/playercontrols/TributeButton~boongui.js
@@ -1,7 +1,10 @@
 DiplomacyDialogPlayerControl.prototype.TributeButtonManager.prototype.TributeButton.prototype.update = function(playerInactive)
 {
-	this.button.hidden = playerInactive || g_Players[g_ViewedPlayer].isEnemy[this.playerID];
+	this.button.hidden = playerInactive;
 
+	// Checkbox implemented because it is sometimes necessary to send resources to my enemy.
+	if (Engine.GetGUIObjectByName("displayEnemyTributeButtons").checked == false)
+		this.button.hidden = playerInactive || g_Players[g_ViewedPlayer].isEnemy[this.playerID];
 	if (!this.button.hidden)
 		this.button.enabled = controlsPlayer(g_ViewedPlayer);
 };
