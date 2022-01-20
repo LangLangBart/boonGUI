435b60438716f5c34a7f664a62f6e28be998528a
Langbart <92653266+LangLangBart@users.noreply.github.com>
2022-01-20 10:53:28 +0100

    Small adaptation of the information label for observers
    Reducing the font size

--- gui/session/top_panel/BuildLabel.js
+++ gui/session/top_panel/BuildLabel.js
@@ -9,15 +9,15 @@ class BuildLabel
 
 		this.viewPlayer = Engine.GetGUIObjectByName("viewPlayer");
 		this.shortGameInfosLabel = Engine.GetGUIObjectByName("shortGameInfosLabel");
-		this.shortGameInfosLabel.caption = Engine.IsAtlasRunning() ? "" : sprintf("%(icon_alpha)s Alpha XXV  %(icon_map)s %(mapName)s%(mapSize)s%(biome)s  %(icon_pop)s %(pop)s  %(rating)s", {
-			"icon_alpha": '[icon="icon_alpha" displace="0 4"]',
+		this.shortGameInfosLabel.caption = Engine.IsAtlasRunning() ? "" : sprintf("%(icon_alpha)s A25  %(icon_map)s %(mapName)s%(mapSize)s%(biome)s  %(icon_pop)s %(pop)s  %(rating)s", {
+			"icon_alpha": '[icon="icon_alpha" displace="1 5"]',
 			"icon_map": '[icon="icon_map" displace="2 6"]',
 			"mapName": this.mapCache.translateMapName(this.mapCache.getTranslatableMapName(g_InitAttributes.mapType, g_InitAttributes.map)),
 			"mapSize": g_InitAttributes.mapType == "random" ? " - " + g_MapSizes.Name[g_MapSizes.Tiles.indexOf(g_InitAttributes.settings.Size)] : "",
 			"biome": g_InitAttributes.settings.Biome ? " - " + g_Settings.Biomes.find(b => b.Id == g_InitAttributes.settings.Biome).Title : "",
 			"icon_pop": '[icon="icon_pop" displace="3 5"]',
 			"pop": g_InitAttributes.settings.PopulationCap !== undefined ? g_PopulationCapacities.Title[g_PopulationCapacities.Population.indexOf(g_InitAttributes.settings.PopulationCap)] : g_WorldPopulationCapacities.Title[g_WorldPopulationCapacities.Population.indexOf(g_InitAttributes.settings.WorldPopulationCap)] + " (WP)",
-			"rating": g_InitAttributes.settings.RatingEnabled === true ? '[icon="icon_rating" displace="-3 5"]' + coloredText("Rated game", "red") : ""
+			"rating": g_InitAttributes.settings.RatingEnabled === true ? '[icon="icon_rating" displace="-3 5"]' + coloredText("Rated", "red") : ""
 		});
 		playerViewControl.registerViewedPlayerChangeHandler(this.onViewedPlayerChange.bind(this));
 	}
