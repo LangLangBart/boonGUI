ed9959938f1391f1d91420f6d82cf3947a977b06
sl5net <sl5softwarelab@gmail.com>
2022-04-21 11:31:26 +0200

    set to main default

--- simulation/components/GuiInterface~boongui.js
+++ simulation/components/GuiInterface~boongui.js
@@ -198,8 +198,6 @@ GuiInterface.prototype.boongui_GetOverlay = function(_, { g_IsObserver, g_Viewed
 		if (updateCache)
 		{
 			cached.civCentres = [];
-			cached.farmstead = [];
-			cached.barrack = [];
 			cached.queue = new CustomQueue();
 			cached.resourcesGathered = cmpPlayerStatisticsTracker?.resourcesGathered || {};
 			cached.percentMapExplored = cmpPlayerStatisticsTracker?.GetPercentMapExplored() ?? 0;
