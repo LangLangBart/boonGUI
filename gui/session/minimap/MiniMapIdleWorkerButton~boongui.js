b154d5911c74b52a11bec6030c0a716b81a25148
sl5net <sl5softwarelab@gmail.com>
2022-04-22 15:12:36 +0200

    beep idle

--- gui/session/minimap/MiniMapIdleWorkerButton~boongui.js
+++ gui/session/minimap/MiniMapIdleWorkerButton~boongui.js
@@ -1,3 +1,4 @@
+let lastBeepTime = 1;
 MiniMapIdleWorkerButton.prototype.rebuild = function()
 {
 	const totalNumberIdleWorkers = Engine.GuiInterfaceCall("FindIdleUnits", {
@@ -6,5 +7,14 @@ MiniMapIdleWorkerButton.prototype.rebuild = function()
 		"excludeUnits": []
 	}).length;
 	this.idleWorkerButton.enabled = totalNumberIdleWorkers > 0;
+	const nowTime = Date.now();
+	const waitedTime = nowTime - lastBeepTime;
+	if(this.idleWorkerButton.enabled) {
+		const multiplier = (totalNumberIdleWorkers > 9) ? 9 : totalNumberIdleWorkers;
+		if( waitedTime * multiplier > 500) {
+			Engine.PlayUISound("audio/interface/alarm/alarm_invalid_building_placement_01.ogg", false);
+			lastBeepTime = Date.now();
+		}
+	}
 	Engine.GetGUIObjectByName("totalNumberIdleWorkers").caption = this.idleWorkerButton.enabled ? totalNumberIdleWorkers : "";
 };
