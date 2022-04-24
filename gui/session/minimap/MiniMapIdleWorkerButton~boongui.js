e02b4d1960450d06cbeb31feb6f0664d676f3858
sl5net <sl5softwarelab@gmail.com>
2022-04-24 13:41:23 +0200

    fix option

--- gui/session/minimap/MiniMapIdleWorkerButton~boongui.js
+++ gui/session/minimap/MiniMapIdleWorkerButton~boongui.js
@@ -7,13 +7,13 @@ MiniMapIdleWorkerButton.prototype.rebuild = function()
 	}).length;
 	this.idleWorkerButton.enabled = totalNumberIdleWorkers > 0;
 
-	const waitedTime = Date.now() - this.lastBeepTime;
-	if(this.idleWorkerButton.enabled && waitedTime * Math.min(totalNumberIdleWorkers, 5) > 10000)
-	{
-		Engine.PlayUISound("audio/interface/alarm/alarm_no_idle_unit_01.ogg", false);
-		this.lastBeepTime = Date.now();
+	if(Engine.ConfigDB_GetValue("user", "boongui.showduration") == "true") {
+		const waitedTime = Date.now() - this.lastBeepTime;
+		if (this.idleWorkerButton.enabled && waitedTime * Math.min(totalNumberIdleWorkers, 5) > 10000) {
+			Engine.PlayUISound("audio/interface/alarm/alarm_no_idle_unit_01.ogg", false);
+			this.lastBeepTime = Date.now();
+		}
 	}
-
 	Engine.GetGUIObjectByName("totalNumberIdleWorkers").caption = this.idleWorkerButton.enabled ? totalNumberIdleWorkers : "";
 };
 
