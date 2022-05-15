656b78eccbadad05e93749538723855aac28c19e
sl5net <sl5softwarelab@gmail.com>
2022-05-15 18:04:42 +0200

    prettify code

--- gui/session/minimap/MiniMapIdleWorkerButton~boongui.js
+++ gui/session/minimap/MiniMapIdleWorkerButton~boongui.js
@@ -1,20 +1,37 @@
-MiniMapIdleWorkerButton.prototype.rebuild = function()
-{
-	const totalNumberIdleWorkers = Engine.GuiInterfaceCall("FindIdleUnits", {
-		"viewedPlayer": g_ViewedPlayer,
-		"idleClasses": this.idleClasses,
-		"excludeUnits": []
-	}).length;
-	this.idleWorkerButton.enabled = totalNumberIdleWorkers > 0;
+// let beepIdleOn = true;
+let beepIdleOn = Engine.ConfigDB_GetValue("user", "boongui.beepIdle") == "true";
+// Engine.ConfigDB_GetValue("user", "boongui.showduration") == "true"
 
-	if(Engine.ConfigDB_GetValue("user", "boongui.showduration") == "true") {
-		const waitedTime = Date.now() - this.lastBeepTime;
-		if (this.idleWorkerButton.enabled && waitedTime * Math.min(totalNumberIdleWorkers, 5) > 10000) {
-			Engine.PlayUISound("audio/interface/alarm/alarm_no_idle_unit_01.ogg", false);
-			this.lastBeepTime = Date.now();
-		}
-	}
-	Engine.GetGUIObjectByName("totalNumberIdleWorkers").caption = this.idleWorkerButton.enabled ? totalNumberIdleWorkers : "";
+MiniMapIdleWorkerButton.prototype.rebuild = function () {
+    const totalNumberIdleWorkers = Engine.GuiInterfaceCall("FindIdleUnits", {
+        "viewedPlayer": g_ViewedPlayer,
+        "idleClasses": this.idleClasses,
+        "excludeUnits": []
+    }).length;
+    this.idleWorkerButton.enabled = totalNumberIdleWorkers > 0;
+
+    if (Engine.ConfigDB_GetValue("user", "boongui.showduration") == "true")
+	{
+        const waitedTime = Date.now() - this.lastBeepTime;
+        // this.idleWorkerButton.enabled &&
+        if (beepIdleOn && waitedTime * Math.min(totalNumberIdleWorkers, 5) > 1000) {
+            Engine.PlayUISound("audio/interface/alarm/beep_idle_02.ogg", false);
+            this.lastBeepTime = Date.now();
+        }
+    }
+    Engine.GetGUIObjectByName("totalNumberIdleWorkers").caption = this.idleWorkerButton.enabled ? totalNumberIdleWorkers : "";
 };
 
+MiniMapIdleWorkerButton.prototype.onKeyDown = function () {
+    // Engine.PlayUISound("audio/interface/alarm/beep_idle_01.ogg", false); // not really needet
+    beepIdleOn = !beepIdleOn;
+}
+
+// this is not working. LeftDoubleClick is a good idea so old functuanallyty dont need be overwritten
+MiniMapIdleWorkerButton.prototype.onMouseLeftDoubleClickItem = function () {
+    Engine.PlayUISound("audio/interface/alarm/beep_idle_01.ogg", false); // not really needet
+    //this.playersBox.onMouseLeftDoubleClickItem = this.onMouseLeftDoubleClickItem.bind(this);
+}
 MiniMapIdleWorkerButton.prototype.lastBeepTime = 0;
+MiniMapIdleWorkerButton.prototype.Tooltip = markForTranslation("idle worker\nNumber of idle workers.\nbeepIdle toggle (On/Off)");
+
