dbd8a8629e78a723df091f29fbcb043e90bfa1ad
LangLangbart <92653266+LangLangBart@users.noreply.github.com>
2022-07-11 21:50:11 +0200

    t er2

--- gui/session/stats/BoonGUIHelpers.js
+++ gui/session/stats/BoonGUIHelpers.js
@@ -35,7 +35,7 @@ const g_BoonGUIResTypes = ["food", "wood", "stone", "metal"];
 function BoonGUIGetRowSize(index, height)
 {
 	const y1 = height * index;
-	const y2 = height * (index + 1)
+	const y2 = height * (index + 1
 	return `0 ${y1} 100% ${y2}`;
 }
 
