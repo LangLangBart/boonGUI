0685722b1ee76119aa99b70c2b4e59731f2b892d
LangLangbart <92653266+LangLangBart@users.noreply.github.com>
2022-10-08 19:31:21 +0200

    reduce the icon size for the infoGameLabel in observer mode

--- gui/session/stats/BoonGUIStats.js
+++ gui/session/stats/BoonGUIStats.js
@@ -310,13 +310,13 @@ class BoonGUIStats
 	{
 		this.mapCache = new MapCache();
 		this.shortGameInfoLabel.caption = Engine.IsAtlasRunning() ? "" : sprintf("%(icon_alpha)s %(AlphaText)s  %(icon_map)s %(mapName)s%(mapSize)s%(biome)s  %(icon_pop)s %(pop)s%(duration)s%(rating)s", {
-			"icon_alpha": '[icon="icon_alpha" displace="1 5"]',
+			"icon_alpha": '[icon="icon_alpha" displace="0 4"]',
 			"AlphaText": g_ProjectInformation.productDescription.caption.match(/A[a-zA-Z ]+/),
-			"icon_map": '[icon="icon_map" displace="2 6"]',
+			"icon_map": '[icon="icon_map" displace="0 6"]',
 			"mapName": this.mapCache.translateMapName(this.mapCache.getTranslatableMapName(g_InitAttributes.mapType, g_InitAttributes.map)),
 			"mapSize": g_InitAttributes.mapType == "random" ? " - " + g_MapSizes.Name[g_MapSizes.Tiles.indexOf(g_InitAttributes.settings.Size)] : "",
 			"biome": g_InitAttributes.settings.Biome ? " - " + g_Settings.Biomes.find(b => b.Id == g_InitAttributes.settings.Biome).Title : "",
-			"icon_pop": '[icon="icon_pop" displace="3 5"]',
+			"icon_pop": '[icon="icon_pop" displace="2 5"]',
 			"pop": g_InitAttributes.settings.PopulationCap !== undefined ? g_PopulationCapacities.Title[g_PopulationCapacities.Population.indexOf(g_InitAttributes.settings.PopulationCap)] : g_WorldPopulationCapacities.Title[g_WorldPopulationCapacities.Population.indexOf(g_InitAttributes.settings.WorldPopulationCap)] + " (WP)",
 			"rating": g_InitAttributes.settings.RatingEnabled === true ? '  [icon="icon_rating" displace="-3 5"]' + coloredText("Rated", "red") : "",
 			"duration": Engine.ConfigDB_GetValue("user", "boongui.showduration") == "true" ? this.durationReplay() : ""
