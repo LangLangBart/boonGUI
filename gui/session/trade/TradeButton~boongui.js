9f6e64a05975626f5f9cd0795c0d8c092ab249c7
LangLangbart <92653266+LangLangBart@users.noreply.github.com>
2023-02-18 05:52:19 +0100

    Store the last trading probabilities for reuse

---
+++ gui/session/trade/TradeButton~boongui.js
@@ -0,0 +1,33 @@
+/**
+ * BoonGUITradeButtonManager class modifies the "constructor" and the "setTradingGoods()" method
+ * of the TradeButtonManager class, allowing to remember the last trading probabilities for
+ * each resource and update if they change.
+ */
+class BoonGUITradeButtonManager extends TradeButtonManager {
+
+	constructor()
+	{
+		super();
+		this.userDefinedPropb = Engine.ConfigDB_GetValue("user", "boongui.session.trade.probability").split(",");
+		// Check if the sum of the numbers is 100% and the number of elements is equal to the number of resources.
+		if (this.userDefinedPropb.reduce((acc, val) => +acc + +val, 0) === 100 && Object.values(this.tradingGoods).length === this.userDefinedPropb.length)
+		{
+			for (const resCode in this.tradingGoods)
+				this.tradingGoods[resCode] = +this.userDefinedPropb.shift();
+			this.setTradingGoods();
+		}
+		else
+			Engine.ConfigDB_CreateAndWriteValueToFile("user", "boongui.session.trade.probability", "", "config/user.cfg");
+	}
+	setTradingGoods()
+	{
+		// save the probability values as string, for example: "0,25,25,50"
+		Engine.ConfigDB_CreateAndWriteValueToFile("user", "boongui.session.trade.probability", Object.values(this.tradingGoods).toString(), "config/user.cfg");
+		Engine.PostNetworkCommand({
+			"type": "set-trading-goods",
+			"tradingGoods": this.tradingGoods
+		});
+	}
+}
+
+TradeButtonManager = BoonGUITradeButtonManager;
